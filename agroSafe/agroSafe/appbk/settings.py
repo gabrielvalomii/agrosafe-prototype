6828f7357cc005792ee7b4d2de9ac0d677d6dace
gabriel valomi <marques.valomi@gmail.com>
2026-06-14 14:01:43 -0300

    ajustando docker compose e conectando db
    criei um db local para rodar o projeto e ajustei o docker compose para usar uma imagem do mysql e deixar o projeto mais proficional

--- agroSafe/agroSafe/appbk/settings.py
+++ agroSafe/agroSafe/appbk/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'appbk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agrosafe',
+        'USER': 'agro_user',
+        'PASSWORD': '13102001g',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
